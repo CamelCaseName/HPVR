668d4b0cc12fd59a1cd3f4cb6918532527ca0b5f
Leonhard Seidel <leonhardseidel@gmx.de>
2026-04-14 23:28:45 +0200

    no crashes, dunno if works

--- HPVR.cs
+++ HPVR.cs
@@ -132,13 +132,10 @@ namespace HPVR
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             //this all looks good, however we still only get one eye. rest works....
-            MelonLogger.Msg("active: " + XRGraphics.enabled);
-            MelonLogger.Msg("loaded: " + XRGraphics.loadedDeviceName);
-            MelonLogger.Msg("eye tex: " + XRGraphics.eyeTextureHeight + " " + XRGraphics.eyeTextureWidth);
-            MelonLogger.Msg("pass: " + XRGraphics.stereoRenderingMode);
-            MelonLogger.Msg("settings: " + XRGeneralSettings.Instance?.name);
-            MelonLogger.Msg("manager settings: " + XRGeneralSettings.Instance?.Manager?.name);
-            MelonLogger.Msg("loader: " + XRGeneralSettings.Instance?.Manager?.activeLoader?.name);
+            //MelonLogger.Msg("active: " + XRGraphics.enabled);
+            //MelonLogger.Msg("loaded: " + XRGraphics.loadedDeviceName);
+            //MelonLogger.Msg("eye tex: " + XRGraphics.eyeTextureHeight + " " + XRGraphics.eyeTextureWidth);
+            //MelonLogger.Msg("pass: " + XRGraphics.stereoRenderingMode);
 
             MelonLogger.Msg("[HPVR] preparing scene " + sceneName);
             inGameMain = sceneName == "GameMain" || sceneName == "ForestEnvironment";
